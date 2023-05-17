61103d5e29a23d646e9c64bdd85373ed64b250b9
@RezaDadashi <dadashireza0@gmail.com>
2023-05-17 11:20:36 +0430

    add modal to bottom of screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: PersianFonts.vazirTextTheme,
+        // primarySwatch: Colors.lightBlue,
+      ),
       home: Scaffold(
         body: MyCustomForm(),
       ),
@@ -17,7 +22,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
+  MyCustomForm({super.key});
+
+  final List<String> items = [
+    'فعال سازی و دریافت رمز عبور',
+    'رمز عبور را فراموش کردم',
+    'افتتاح حساب می کنم'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +46,7 @@ class MyCustomForm extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Container(
-                padding: const EdgeInsets.only(top: 50.0, bottom: 35.0),
+                padding: const EdgeInsets.only(top: 30.0, bottom: 20.0),
                 width: 100,
                 child: Image.asset('images/ttbank.png'),
               ),
@@ -44,10 +55,13 @@ class MyCustomForm extends StatelessWidget {
                 child: TextField(
                   textAlign: TextAlign.right,
                   decoration: InputDecoration(
-                      label: Text(
-                    'نام کاربری *',
-                    style: PersianFonts.Vazir,
-                  )),
+                    label: Center(
+                      child: Text(
+                        'نام کاربری',
+                        // style: PersianFonts.Vazir,
+                      ),
+                    ),
+                  ),
                 ),
               ),
               const Padding(
@@ -55,28 +69,23 @@ class MyCustomForm extends StatelessWidget {
                 child: TextField(
                   textAlign: TextAlign.right,
                   decoration: InputDecoration(
-                    label: Text(
-                      'رمز عبور *',
-                      style: PersianFonts.Vazir,
+                    label: Center(
+                      child: Text(
+                        'رمز عبور',
+                        // style: PersianFonts.Vazir,
+                      ),
                     ),
                   ),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 25.0),
-                child: Text(
-                  'فراموشی رمز عبور',
-                  style: PersianFonts.Yekan,
-                ),
-              ),
               Container(
                 width: double.infinity,
-                padding: const EdgeInsets.only(top: 40.0),
+                padding: const EdgeInsets.only(top: 30.0),
                 child: ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
                     elevation: 10,
-                    backgroundColor: const Color.fromARGB(255, 37, 128, 128),
+                    backgroundColor: const Color.fromARGB(255, 10, 80, 80),
                     padding: const EdgeInsets.symmetric(
                         horizontal: 15.0, vertical: 15.0),
                   ),
@@ -86,40 +95,116 @@ class MyCustomForm extends StatelessWidget {
                       Icon(Icons.mobile_friendly_rounded),
                       Text(
                         'ورود',
-                        style: PersianFonts.Vazir,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Column(
+                  children: const [
+                    Icon(
+                      Icons.fingerprint_rounded,
+                      color: Colors.red,
+                      size: 50,
+                    ),
+                    Text(
+                      'ورود با اثر انگشت',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: InkWell(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          padding: const EdgeInsets.all(12.0),
+                          height: 200.0,
+                          decoration: const BoxDecoration(
+                            borderRadius:
+                                BorderRadius.vertical(top: Radius.circular(30)),
+                          ),
+                          child: ListView.builder(
+                              itemCount: items.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  // crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    ListTile(
+                                      title: Center(
+                                        child: Text(
+                                          items[index],
+                                        ),
+                                      ),
+                                    ),
+                                    const Divider(
+                                      height: 10,
+                                      thickness: 1,
+                                      // indent: ,
+                                      // endIndent: 0,
+                                      color: Color(0xFF54c5d0),
+                                    ),
+                                  ],
+                                );
+                              }),
+                        );
+                      },
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'افتتاح حساب',
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text('/'),
+                      ),
+                      Text(
+                        'فراموشی رمز',
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text('/'),
+                      ),
+                      Text(
+                        'فعالسازی',
                       ),
                     ],
                   ),
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(top: 70.0),
+                width: 170.0,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                margin: const EdgeInsets.only(top: 20.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF54c5d0),
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: const [
-                        Icon(
-                          Icons.location_on,
-                          color: Color(0xFF3ec6c2),
-                        ),
-                        Text(
-                          'شعبات نزدیک به من',
-                          style: PersianFonts.Vazir,
-                        ),
-                      ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'شعبات نزدیک به من',
+                      style: TextStyle(
+                        color: Colors.white70,
+                      ),
                     ),
-                    Column(
-                      children: const [
-                        Icon(
-                          Icons.account_balance_rounded,
-                          color: Color(0xFF3ec6c2),
-                        ),
-                        Text(
-                          'افتتاح حساب',
-                          style: PersianFonts.Vazir,
-                        ),
-                      ],
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.grey,
                     ),
                   ],
                 ),
@@ -131,3 +216,57 @@ class MyCustomForm extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Page Transition',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Page'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: const Text('Go to Modal'),
+//           onPressed: () {
+//             showModalBottomSheet(
+//               context: context,
+//               builder: (BuildContext context) {
+//                 return SizedBox(
+//                   height: 300.0,
+//                   child: ElevatedButton(
+//                     child: const Text('close'),
+//                     onPressed: () {
+//                       Navigator.pop(context);
+//                     },
+//                   ),
+//                 );
+//               },
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
